b241d37e7e73e039d03d80fded5dca3a03b4f5c3
Luong Quang Duy <duylw25@gmail.com>
2025-06-20 23:36:56 +0700

    feature: flight_ticket_class api setup

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from api.admin_api import admin_bp
 from api.flight_api import flight_bp
 from api.airport_api import airport_bp
 from api.ticket_class_api import ticket_class_bp
+from api.flight_ticket_class_api import flight_ticket_class_bp
 from app import init_app
 from config.app_config import AppConfig
 
@@ -26,5 +27,6 @@ app.register_blueprint(admin_bp, url_prefix='/api/v1/admin')
 app.register_blueprint(flight_bp, url_prefix='/api/v1/flight')
 app.register_blueprint(airport_bp, url_prefix='/api/v1/airport')
 app.register_blueprint(ticket_class_bp, url_prefix='/api/v1/ticket-class')
+app.register_blueprint(flight_ticket_class_bp, url_prefix='/api/v1/flight-ticket-class')
 
 app.register_blueprint(index_bp, url_prefix='')
